9452883253486d694ef425735c9c8abcb9ff738f
cboto <catarinamaralboto@gmail.com>
2021-09-24 23:54:12 +0100

    Geolocation features

---
+++ Encounter Me/Geolocation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Encounter_Me
+{
+    /*
+     * Geolocates the user using Geolocation.js
+     * 
+     */
+    public class Geolocation
+    {
+        private readonly IJSRuntime js;
+
+        public Geolocation(IJSRuntime js)
+        {
+            this.js = js;
+        }
+        public async ValueTask GetUserCoords()
+        {
+            //Runs the JS script that asks the browser for the location.
+            await js.InvokeVoidAsync("Geolocate");
+        }
+
+       
+    }
+}
