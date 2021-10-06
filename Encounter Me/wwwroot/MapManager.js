06baab3ac96e0f58dcf247bdf7546872e11a9ff3
R <mikrytis@gmail.com>
2021-10-06 12:05:53 +0300

    Cleanup, ability to load multiple markers

--- Encounter Me/wwwroot/MapManager.js
+++ Encounter Me/wwwroot/MapManager.js
@@ -1,11 +1,12 @@
 ﻿var map;
-var markers;
+var markers = [];
 var geojsonLayer;
 
 function initialize(Lat, Lng)
 {
-    var mymap = L.map('mapid').setView([Lat, Lng], 15);
+    map = L.map('mapid').setView([Lat, Lng], 15);
 
+    //mapbox map
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         maxZoom: 18,
         attribution: "&copy; <a lhref='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>",
@@ -13,28 +14,22 @@ function initialize(Lat, Lng)
         zoomOffset: -1,
         id: "mapbox/outdoors-v11",
         accessToken: 'pk.eyJ1Ijoicnl0aXNvayIsImEiOiJja3R3b20wbGEybTl3MzBtcGdhZG96MnhqIn0.GIdSHPlZoTkmFHavTZZOqQ'
-    }).addTo(mymap);
+    }).addTo(map);
+
+    //default open street maps map - blurry
     /*L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: "&copy; <a lhref='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>",
         tileSize: 512,
         zoomOffset: -1,
     }).addTo(mymap);*/
-    L.control.scale({ imperial: true, metric: true }).addTo(mymap);
-    map = mymap;
+
+    L.control.scale({ imperial: true, metric: true }).addTo(map);
 
     map.on('zoomend', function ()
     {
-        var currentZoom = mymap.getZoom();
-        var icon = new L.Icon({
-            iconUrl: 'footprint.png',
-            iconSize: [currentZoom * 4, currentZoom * 4],
-            iconAnchor: [0, 0],
-            popupAnchor: [15, (-50 / currentZoom)]
-        });
-        markers.setIcon(icon);
+        updateMarkerSize();
     });
-
 }
 
 function addMarker(Lat, Lng, text, geoJsonUrl)
@@ -47,27 +42,30 @@ function addMarker(Lat, Lng, text, geoJsonUrl)
             popupAnchor: [15, -50]
         }
     });
-
+    //creates new marker with icon and specified coords
     var markerIcon = new icon({ iconUrl: 'footprint.png' });
     var marker = L.marker([Lat, Lng], { icon: markerIcon });
+
     //assign trail information text
     marker.bindPopup(text);
+
     //show geojson on marker click
     marker.on('click',
     function (e) {
-        markerOnClick(geoJsonUrl);
-    });
+        showGeojson(geoJsonUrl);
+        });
+
     //hide geojson on pop up close
     marker.getPopup().on('remove', function () {
         geojsonLayer.clearLayers();
     });
-    marker.addTo(map);;
 
-    markers = marker;
-
+    marker.addTo(map);
+    markers.push(marker);
 }
 
-function markerOnClick(geoJsonUrl)
+//loads and displays geojson with the specified name
+function showGeojson(geoJsonUrl)
 {
     fetch(geoJsonUrl)
         .then(function (response) {
@@ -77,4 +75,16 @@ function markerOnClick(geoJsonUrl)
             geojsonLayer = L.geoJSON(data);
             geojsonLayer.addTo(map);
         });
+}
+
+//updates all marker sizes based on zoom level
+function updateMarkerSize() {
+    var currentZoom = map.getZoom();
+    var icon = new L.Icon({
+        iconUrl: 'footprint.png',
+        iconSize: [currentZoom * 4, currentZoom * 4],
+        iconAnchor: [0, 0],
+        popupAnchor: [15, (-50 / currentZoom)]
+    });
+    markers.forEach(element => element.setIcon(icon));
 }
