9ca3c226941293dc1d5674963aa938e7e6a8f2c0
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-10-06 11:28:50 +0300

    Create UserData.cs
    This class is meant to store User data. Decided against Level or ExperiencePoints validation using properties as it would not comply with SRP and OCP.

---
+++ Encounter Me/UserData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Encounter_Me.Shared
+{
+    public class UserData
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserPhotoUrl { get; set; }
+
+        public int Id { get; set; }
+        public int Level { get; set; }
+        public double ExperiencePoints { get; set; }
+
+    }
+}
