7c9f9a70d28a78a6fbcf7ccc64f009318effdf2d
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-10-10 19:24:13 +0300

    Update UserData.cs
    Added a constructor.

--- Encounter Me/UserData.cs
+++ Encounter Me/UserData.cs
@@ -7,15 +7,22 @@ namespace Encounter_Me.Shared
 {
     public class UserData
     {
-
+        public int Id { get; set; } // Could be a string 
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string UserPhotoUrl { get; set; }
 
-        public int Id { get; set; }
         public int Level { get; set; }
         public double ExperiencePoints { get; set; }
 
+        public UserData(int id, string firstName, string lastName, string email, string userPhotoUrl)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            UserPhotoUrl = userPhotoUrl;
+        }
     }
 }
