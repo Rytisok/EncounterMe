04fea4da318682132bb25c83e10b066ce0b3ddfa
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-10-18 21:33:09 +0300

    Create Registration Form
    Created registration form with validation of the fields. Still need to finish the work and add input data fetching.

---
+++ Encounter Me/Controllers/UserDataController.cs
@@ -0,0 +1,38 @@
+﻿//using Microsoft.AspNetCore.Http;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.Web.Http;
+//using System.Web.Mvc;
+
+//namespace Encounter_Me.Controllers
+//{
+//    [System.Web.Http.Route("api/[controller]")]
+//    [ApiController]
+//    public class UserDataController : ControllerBase
+//    {
+//        readonly List<string> userNameList = new();
+
+//        public UserDataController()
+//        {
+//            userNameList.Add("ankit");
+//            userNameList.Add("vaibhav");
+//            userNameList.Add("priya");
+//        }
+
+//        [System.Web.Http.HttpPost]
+//        public IActionResult Post(UserRegistration registrationData)
+//        {
+//            if (userNameList.Contains(registrationData.Username.ToLower()))
+//            {
+//                ModelState.AddModelError(nameof(registrationData.Username), "This User Name is not available.");
+//                return BadRequest(ModelState);
+//            }
+//            else
+//            {
+//                return Ok(ModelState);
+//            }
+//        }
+//    }
+//}
---
+++ Encounter Me/Shared/UserRegistrationModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Encounter_Me.Shared
+{
+    public class UserRegistrationModel
+    {
+        [Required, StringLength(30, ErrorMessage ="Username must be shorter than 30 characters")]
+        public string UserName { get; set; }
+        [Required]
+        [RegularExpression(@"^\p{Lu}[ \p{L}'-]*[\p{Ll}]$",
+            ErrorMessage = "Name must start with a capital letter and it can not end with a space symbol")]
+        public string FirstName { get; set; }
+        [Required]
+        [RegularExpression(@"^\p{Lu}[ \p{L}'-]*[\p{Ll}]$",
+            ErrorMessage = "Surname must start with a capital letter")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression(@" ^ (?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$",
+            ErrorMessage = "Password should have at least 8 characters,  1 uppercase letter, 1 lowercase letter and 1 number.")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Confirm password and Password fields values do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
