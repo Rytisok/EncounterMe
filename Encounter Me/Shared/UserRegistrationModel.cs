fd9d572631ec76bf4996dbb1dc629e5a36e47e10
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-10-18 23:27:09 +0300

    Fix password validation
    The regex was not working properly. Fixed it, turned off autocompletion for the fields.

--- Encounter Me/Shared/UserRegistrationModel.cs
+++ Encounter Me/Shared/UserRegistrationModel.cs
@@ -9,18 +9,19 @@ namespace Encounter_Me.Shared
         public string UserName { get; set; }
         [Required]
         [RegularExpression(@"^\p{Lu}[ \p{L}'-]*[\p{Ll}]$",
-            ErrorMessage = "Name must start with a capital letter and it can not end with a space symbol")]
+            ErrorMessage = "Name must start with a capital letter, and have only letters")]
         public string FirstName { get; set; }
         [Required]
         [RegularExpression(@"^\p{Lu}[ \p{L}'-]*[\p{Ll}]$",
-            ErrorMessage = "Surname must start with a capital letter")]
+            ErrorMessage = "Surname must start with a capital letter and have only letters")]
         public string LastName { get; set; }
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required]
-        [RegularExpression(@" ^ (?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$",
-            ErrorMessage = "Password should have at least 8 characters,  1 uppercase letter, 1 lowercase letter and 1 number.")]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$",
+            ErrorMessage = "Password should have at least 8 characters, one uppercase letter, one lowercase letter and a number.")]
         public string Password { get; set; }
 
         [Required]
