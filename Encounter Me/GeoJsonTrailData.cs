e4a77abf41943f5aaeb7fe8fe11b3b791df33240
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-10-26 11:31:23 +0300

    Fetch coordinates from geojson
    Very basic functionality needed to get trail coordinates from geojson.  Should be done server-side, not on the client.

---
+++ Encounter Me/GeoJsonTrailData.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Encounter_Me
+{
+    //public class GeoJsonTrailData
+    //{ 
+    //    public RooTobject AccessTrailData { get; set; }
+    //}
+
+    public class RooTobject
+    {
+        public string Type { get; set; }
+        public Feature[] Features { get; set; }
+    }
+    public class Feature
+    {
+        public string Type { get; set; }
+        public Properties Properties { get; set; }
+        public Geometry Geometry { get; set; }
+    }
+
+    public class Properties
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+
+    public class Geometry
+    {
+        public string Type { get; set; }
+        public float[][] Coordinates { get; set; }
+    }
+
+}
+
