6df4340ba48cae25e59e678a224e6620937d77f8
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-11-14 21:47:39 +0200

    Create Achievement.cs

---
+++ Encounter Me/DataClasses/Achievement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Encounter_Me.DataClasses
+{
+    public class Achievement
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
