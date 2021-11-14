45d66abafe4513f9c71d4b9df8c8aa24331e9889
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-11-14 12:45:27 +0200

    clean up dependencies
    Uninstalled unused packages.

--- Encounter Me.Api/Startup.cs
+++ Encounter Me.Api/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
