7fd307742bfd69396569b8adacfcac372c9615ea
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-11-14 12:39:15 +0200

    update login
    now to login user has to provide not username but email. Only for testing purposes.

--- Encounter Me/Models/AuthenticatedUserModel.cs
+++ Encounter Me/Models/AuthenticatedUserModel.cs
@@ -7,7 +7,16 @@ namespace Encounter_Me.Models
 {
     public class AuthenticatedUserModel
     {
+
         public string Access_Token { get; set; }
         public string UserName { get; set; }
+        public AuthenticatedUserModel(string access_Token, string userName)
+        {
+            Access_Token = access_Token;
+            UserName = userName;
+        }
+        public AuthenticatedUserModel()
+        {
+        }
     }
 }
--- Encounter Me/Models/AuthenticationUserModel.cs
+++ Encounter Me/Models/AuthenticationUserModel.cs
@@ -8,8 +8,8 @@ namespace Encounter_Me.Models
 {
     public class AuthenticationUserModel // use as credentials during authentication process
     {
-        [Required(ErrorMessage ="Please provide Username.")]
-        public string UserName { get; set; }
+        [Required(ErrorMessage ="Please provide Email.")]
+        public string Email { get; set; }
         [Required(ErrorMessage ="Please provide password.")]
         public string Password { get; set; }
     }
