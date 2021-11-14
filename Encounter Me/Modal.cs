103c93b050ea003f6d8c9ef3cca0d85421e23ecd
R <mikrytis@gmail.com>
2021-11-15 00:56:34 +0200

    Modal opening and closing cleanup

---
+++ Encounter Me/Modal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Modal
+{
+	public string modalDisplay { get; set; }
+	public string modalClass { get; set; }
+
+	public Modal()
+	{
+		modalDisplay = "none;";
+		modalClass = "";
+	}
+	public Modal(string modalDisplay, string modalClass)
+	{
+		this.modalDisplay = modalDisplay;
+		this.modalClass = modalClass;
+	}
+}
---
+++ Encounter Me/ModalControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class ModalControl
+{
+	public static void OpenModal(Modal modal)
+    {
+        modal.modalDisplay = "block;";
+        modal.modalClass = "show";
+    }
+    public static void CloseModal(Modal modal)
+    {
+        modal.modalDisplay = "";
+        modal.modalClass = "hide";
+    }
+}
