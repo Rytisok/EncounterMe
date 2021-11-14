4b6f483e12f71e5f7452cc96bfd1decde892a233
R <mikrytis@gmail.com>
2021-11-14 23:14:19 +0200

    Device validation moved to separate class

---
+++ Encounter Me/DeviceNotSupportedException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class DeviceNotSupportedException : Exception
+{
+    public DeviceNotSupportedException()
+    {
+
+    }
+    public DeviceNotSupportedException(string message)
+        : base(message)
+    {
+    }
+}
