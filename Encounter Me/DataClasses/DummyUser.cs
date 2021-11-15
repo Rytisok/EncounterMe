98e4f4d347b349063a85e14ed890f0ffae5214ba
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-11-15 19:57:20 +0200

    test achievements on UI
    Created a class to test how achievements would be rendered and accessed. Also tried out how lazy initialization works before transferring this logic to our main UserData class. It was inconvienient to work with it on the go as it is huge and tied to DB.

---
+++ Encounter Me/DataClasses/DummyUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Encounter_Me.DataClasses
+{
+    public class DummyUser
+    {
+        public int ID { get; set; }
+        public string Name { get;  set; }
+        private Lazy<List<Achievement>> _achievements;
+
+        public DummyUser(int userId, string userName)
+        {
+            ID = userId;
+            Name = userName;
+
+            _achievements = new Lazy<List<Achievement>>(() =>
+            {
+                // get from "DB" 
+                return GetAchievementsByID(this.ID);
+            });
+
+        }
+        public List<Achievement> Achievements 
+        {
+            get
+            {
+                return _achievements.Value;
+            }
+        }
+
+        public List<Achievement> GetAchievementsByID(int ID)
+        {
+            return Achievement.GetAchievements();
+        }
+    }
+}
+
--- Encounter Me/Pages/Profile.cs
+++ Encounter Me/Pages/Profile.cs
@@ -1,4 +1,5 @@
-﻿using Encounter_Me.Services;
+﻿using Encounter_Me.DataClasses;
+using Encounter_Me.Services;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
@@ -13,16 +14,17 @@ namespace Encounter_Me.Pages
         [Parameter]
         public string Id { get; set; }
         public UserData User { get; set; } = new UserData();
+        private DummyUser dummyUser = new DummyUser(1, "username");
 
         [Inject]
-
         public IUserDataService UserDataService { get; set; }
 
         protected async override Task OnInitializedAsync()
         {
 
             User = await UserDataService.GetUserDetails(int.Parse(Id));
-
+            dummyUser.ID = User.Id;
+            dummyUser.Name = User.UserName;
         }
     }
 }
