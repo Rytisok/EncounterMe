4ef6adcf2f2653a45ac10349542dea8649b77c13
R <mikrytis@gmail.com>
2021-12-02 19:58:43 +0200

    Random trail generation
    Random trail generation added to API
    Now hardcoded trail directions for each trail marker will be ignored, instead, a random trail, based on difficulty will be generated
    
    How trail generation works:
    -Trail center point is given to function and 2 random points around it are generated
    -Google maps directions api connects first random point, center point, second random point and returns encoded direction polyline
    -Direction polyline is decoded into geojson

---
+++ Encounter Me.Api/Trail generation Utilities/Coordinate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Encounter_Me.Api.Trail_generation_Utilities
+{
+    public class Coordinate
+    {
+        public double Lat, Lon;
+
+        public Coordinate(double Lat,
+                    double Lon)
+        {
+            this.Lat = Lat;
+            this.Lon = Lon;
+        }
+
+        public override string ToString()
+        {
+            return Lat.ToString() + "," + Lon.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Coordinate coordB = obj as Coordinate;
+
+            if (coordB == null)
+            {
+                return false;
+            }
+            else
+            {
+                return (coordB.Lat == this.Lat && coordB.Lon == this.Lon);
+            }
+        }
+    }
+
+}
