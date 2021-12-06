07144f383bc46cb619e4c7f0d5eb97710f30754f
R <mikrytis@gmail.com>
2021-12-06 23:53:56 +0200

    Capture points added to DB
    -CapturePoint class replaced TrailContainer class
    -CapturePoint service added
    -CapturePoint API controller added
    -Capture points from procedurally generated capture points json file now are added to database on build
    -Index page now loads capture points from database based on viewport
    -New capture point icons
    -Trail path generation algorithm adjusted

--- Encounter Me.Api/Migrations/20211206173037_initial.Designer.cs
+++ Encounter Me.Api/Migrations/20211206173037_initial.Designer.cs
@@ -4,14 +4,16 @@ using Encounter_Me.Api.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Encounter_Me.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211206173037_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Encounter Me/Services/ICapturePointService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using BrowserInterop.Geolocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encounter_Me.Services
+{
+    public interface ICapturePointService
+    {
+        Task<IEnumerable<CapturePoint>> GetCapturePointsInView(double Lat1, double Lon1, double Lat2, double Lon2);
+
+        Task<CapturePoint> GetCapturePoint(Guid Id);
+
+        Task UpdateCapturePoint(CapturePoint capturePoint);
+    }
+}
