37b190b93861cf3658e5bad01744882af631e2a4
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-12-12 00:38:46 +0200

    Add code for creating integration tests
    Added code which creates httpClient and sql server database meant to be used in integration tests. DatabaseFixture class ensures that all tests share the same database (transactions need to be used to keep the same state if the test needs to modify it).

---
+++ EncounterMe_IntegrationTests/Helpers/DatabaseFixture.cs
@@ -0,0 +1,75 @@
+﻿using Encounter_Me;
+using Encounter_Me.Api.Models;
+using EncounterMe_IntegrationTests.Helpers;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncounterMe_IntegrationTests
+{
+    public class DatabaseFixture : IDisposable
+    {
+
+        private static readonly object _lock = new object();
+        private static bool _databaseInitialized;
+
+        public DbConnection Connection { get; }
+        public AppDbContext DbContext { get; set; }
+
+        public DatabaseFixture()
+        {
+            Connection = new SqlConnection($"Server=(localdb)\\mssqllocaldb;Database=Encounter_Me_Tests;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            Seed();
+
+            Connection.Open();
+        }
+
+        public AppDbContext CreateContext(DbTransaction transaction = null)
+        {
+            var testingDb = new AppDbContext(new DbContextOptionsBuilder<AppDbContext>().UseSqlServer(Connection).Options);
+
+            if (transaction != null)
+            {
+                testingDb.Database.UseTransaction(transaction);
+            }
+
+            return testingDb;
+        }
+
+        private void Seed()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var db = CreateContext())
+                    {
+                        db.Database.EnsureDeleted();
+                        db.Database.EnsureCreated();
+
+                        Utilities.InitializeDbForTests(db);
+                        DbContext = db;
+                    }
+
+                    _databaseInitialized = true;
+                }
+            }
+        }
+
+        public void ForceDestroyDatabase()
+        {
+            Utilities.DropDb(DbContext);
+        }
+        public void Dispose()
+        {
+            Connection.Close();
+            Connection.Dispose();
+        }
+    }
+}
---
+++ EncounterMe_IntegrationTests/Helpers/Utilities.cs
@@ -0,0 +1,51 @@
+﻿using Encounter_Me;
+using Encounter_Me.Api.Models;
+using Encounter_Me.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncounterMe_IntegrationTests.Helpers
+{
+    class Utilities
+    {
+        public static void InitializeDbForTests(AppDbContext db)
+        {
+            db.Users.AddRange(GetSeedingUsers());
+            db.SaveChanges();
+        }
+
+        public static List<UserData>GetSeedingUsers()
+        {
+            var hashSalt = PasswordManager.EncryptPassword("Testing1");
+
+
+            return new List<UserData>()
+            {
+                new UserData(Guid.Parse("d68ba044-95a1-442b-8754-e329410c766b"), (Factions)2, "Username", "Fn",  "Ln",  "userName@mail", hashSalt.Hash, hashSalt.Salt),
+                new UserData(Guid.NewGuid(), (Factions)1, "Rr", "Fn",  "Ln",  "Rr@mail", hashSalt.Hash, hashSalt.Salt),
+                new UserData(Guid.NewGuid(), (Factions)3, "Dm", "Fn",  "Ln",  "Dd@mail", hashSalt.Hash, hashSalt.Salt)
+            };
+        }
+
+        //public static void ReinitializeDbForTests(AppDbContext db)
+        //{
+        //    db.Users.RemoveRange(db.Users);
+        //    InitializeDbForTests(db);
+        //}
+
+        //public static void ReinitializeEmptyDbForTests(AppDbContext db)
+        //{
+        //    db.Users.RemoveRange(db.Users);
+        //    db.Trails.RemoveRange(db.Trails);
+        //    db.SaveChanges();
+        //}
+
+        //public static void DropDb(AppDbContext db)
+        //{
+        //    db.Database.EnsureDeleted();
+        //}
+    }
+}
