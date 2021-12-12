60b1a4cc01fb994cf2e510ff15eec226d9a95d49
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-12-12 17:35:00 +0200

    Update TestingWebApplicationFactory.cs
    Cleaned up code.

--- EncounterMe_IntegrationTests/Helpers/TestingWebApplicationFactory.cs
+++ EncounterMe_IntegrationTests/Helpers/TestingWebApplicationFactory.cs
@@ -1,21 +1,15 @@
 ﻿using Encounter_Me.Api.Models;
-using EncounterMe_IntegrationTests.Helpers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace EncounterMe_IntegrationTests
 {
     public class TestingWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup: class
     {
-
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureServices(services =>
@@ -23,10 +17,8 @@ namespace EncounterMe_IntegrationTests
                 var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
 
                 services.Remove(descriptor);
-
-
                 services.AddDbContext<AppDbContext>(options =>
-                                  options.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Database=Encounter_Me_Tests;Trusted_Connection=True;MultipleActiveResultSets=true"));
+                         options.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Database=Encounter_Me_Tests;Trusted_Connection=True;MultipleActiveResultSets=true"));
 
                 var sp = services.BuildServiceProvider();
 
@@ -45,7 +37,6 @@ namespace EncounterMe_IntegrationTests
                     }   
                 }
             });
-
         }
     }
 }
