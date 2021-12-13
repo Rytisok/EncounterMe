f57285f0e27bd28b05a53c412db1ba6ea3aec7f9
ZagreDo <71849135+ZagreDo@users.noreply.github.com>
2021-12-13 20:52:59 +0200

    Minor fixes

---
+++ Encounter Me/Pages/EditProfile.cs
@@ -0,0 +1,37 @@
+﻿using Encounter_Me.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Encounter_Me.Pages
+{
+    public partial class EditProfile
+    {
+        [Parameter]
+        public string Id { get; set; }
+        public UserData User { get; set; } = new UserData();
+
+        [Inject]
+
+        public IUserDataService UserDataService { get; set; }
+
+        [Inject]
+        public NavigationManager NavManager{ get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+
+            User = await UserDataService.GetUserDetails(Guid.Parse(Id));
+
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            await UserDataService.UpdateUser(User);
+            NavManager.NavigateTo("/");
+        }
+    }
+}
