db2596585cf1ffd544910e0e7ff887141dfa6ba3
ZagreDo <71849135+ZagreDo@users.noreply.github.com>
2021-12-14 09:16:40 +0200

    Added some filtering in leaderboard.

--- Encounter Me/Pages/Leaderboard.cs
+++ Encounter Me/Pages/Leaderboard.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Encounter_Me.Services;
+using Encounter_Me.Shared;
 using Microsoft.AspNetCore.Components;
 
 namespace Encounter_Me.Pages
@@ -11,6 +12,11 @@ namespace Encounter_Me.Pages
     public partial class Leaderboard
     {
         public IEnumerable<UserData> Users { get; set; }
+        public IEnumerable<UserData> UsersByFive { get; set; }
+        public IEnumerable<IGrouping<int, UserData>> FctionsGroup;
+
+        int pageNumber = 1;
+        int nr = 1;
 
         [Inject]
         public IUserDataService UserDataService { get; set; }
@@ -18,6 +24,39 @@ namespace Encounter_Me.Pages
         protected async override Task OnInitializedAsync()
         {
             Users = (await UserDataService.GetAllUsers()).ToList();
+            await OrderUsersByExp();
+            //await ShowFactionsMembers(Factions.Yellow);
+            await doUsers5(pageNumber);
         }
+
+        public async Task OrderUsersByExp()
+        {
+            
+            Users = (from usr in Users
+                     orderby  usr.Level descending, usr.ExperiencePoints descending
+                     select usr).ToList();
+        }
+
+
+        public async Task GivePositionsToUsers()
+        {
+            /*IEnumerable<UserData> temp;
+            int ntemp = (from usr in Users
+                    select usr).Count();
+            temp =(from usr in Users
+                   select new var{ });*/
+        }
+        public async Task doUsers5(int pageNmbr)
+        {
+
+                UsersByFive = (from usr in Users
+                               select usr).Take(pageNmbr * 5).ToList();
+
+                UsersByFive = (from usr in UsersByFive
+                           select usr).Skip((pageNmbr-1) * 5).ToList();           
+
+        }
+
+
     }
 }
---
+++ Encounter Me/Pages/LeaderboardF.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Encounter_Me.Services;
+using Encounter_Me.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace Encounter_Me.Pages
+{
+
+    public partial class LeaderboardF
+    {
+        public IEnumerable<UserData> Users { get; set; }
+        public IEnumerable<IGrouping<int,UserData>> FctionsGroup { get; set; }
+
+        Factions x;
+
+        [Inject]
+        public IUserDataService UserDataService { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            Users = (await UserDataService.GetAllUsers()).ToList();
+            await ShowFactionsMembers(x);
+        }
+
+
+        public async Task ShowFactionsMembers(Factions x)
+        {
+            FctionsGroup = (from usr in Users
+                            orderby usr.Level descending, usr.ExperiencePoints descending
+                            group usr by (int)usr.Faction into faction
+                            where faction.Key == (int)x
+                            select faction);
+
+        }
+
+
+
+    }
+}
