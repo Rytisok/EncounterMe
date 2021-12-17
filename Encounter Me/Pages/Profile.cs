f99de4f0d387ad264f2f72093cd05bf6283ea576
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-12-17 13:29:39 +0200

    Hide editProfile btn in profile page
    Now the button is hidden for everyone except the profile owner himself.

--- Encounter Me/Pages/Profile.cs
+++ Encounter Me/Pages/Profile.cs
@@ -1,8 +1,10 @@
 ﻿using Encounter_Me.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Encounter_Me.Pages
@@ -18,11 +20,23 @@ namespace Encounter_Me.Pages
 
         public IUserDataService UserDataService { get; set; }
 
+        [Inject]
+        public AuthenticationStateProvider AuthStateProvider { get; set; }
+        private bool isAuthenticated = false;
+        private Guid userId;
+
         protected async override Task OnInitializedAsync()
         {
 
             User = await UserDataService.GetUserDetails(Guid.Parse(Id));
+            var authState = await AuthStateProvider.GetAuthenticationStateAsync();
 
+            if (authState.User.Identity.IsAuthenticated)
+            {
+                isAuthenticated = true;
+
+                userId = Guid.Parse(authState.User.FindFirst(ClaimTypes.UserData).Value);
+            }
         }
     }
 }
