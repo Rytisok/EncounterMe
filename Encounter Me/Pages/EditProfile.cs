382b9908eb6fbe7f584b7d7a9de3314384ea0258
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-12-17 13:27:41 +0200

    Restrict editProfile page
    Now only users themselves can see their `editProfile` page. The page is restricted for anonymous users and other users as well.

--- Encounter Me/Pages/EditProfile.cs
+++ Encounter Me/Pages/EditProfile.cs
@@ -1,8 +1,11 @@
-﻿using Encounter_Me.Services;
+﻿using Encounter_Me.Authentication;
+using Encounter_Me.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 
@@ -21,11 +24,24 @@ namespace Encounter_Me.Pages
         [Inject]
         public NavigationManager NavManager{ get; set; }
 
+        [Inject]
+        public AuthenticationStateProvider AuthStateProvider { get; set; }
+
+        private bool isAuthenticated = false;
+        private Guid userId;
         protected async override Task OnInitializedAsync()
         {
 
             User = await UserDataService.GetUserDetails(Guid.Parse(Id));
 
+            var authState = await AuthStateProvider.GetAuthenticationStateAsync();
+
+            if (authState.User.Identity.IsAuthenticated)
+            {
+                isAuthenticated = true;
+
+                userId = Guid.Parse(authState.User.FindFirst(ClaimTypes.UserData).Value);
+            }
         }
 
         protected async Task HandleValidSubmit()
