821af7b5815f8f4f2f9d6ea8134b14bb4392c806
R <mikrytis@gmail.com>
2021-12-17 18:44:53 +0200

    profile paged style improved

--- Encounter Me/Pages/Profile.cs
+++ Encounter Me/Pages/Profile.cs
@@ -4,12 +4,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Encounter_Me.Shared;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Encounter_Me.Pages
 {
     public partial class Profile
     {
-        
         [Parameter]
         public string Id { get; set; }
         public UserData User { get; set; } = new UserData();
@@ -18,11 +21,57 @@ namespace Encounter_Me.Pages
 
         public IUserDataService UserDataService { get; set; }
 
+        private string imageBorderGradient = "";
+        private string primaryColor = "";
+        private string secondaryColor = "";
+        private bool canEdit = false;
+
+        [CascadingParameter]
+        public Task<AuthenticationState> AuthState { get; set; }
+        private AuthenticationState authState;
+
+        [Inject]
+        public IUserDataService userDataService { get; set; }
+
         protected async override Task OnInitializedAsync()
         {
+            authState = await AuthState;
 
             User = await UserDataService.GetUserDetails(Guid.Parse(Id));
 
+            if (authState.User.Identity.IsAuthenticated)
+            {
+                Guid userId = Guid.Parse(authState.User.FindFirst(ClaimTypes.UserData).Value);
+                UserData currentUser = await userDataService.GetUserDetails(userId);
+                if (currentUser.Id.Equals(User.Id))
+                {
+                    canEdit = true;
+                }
+            } 
+
+            switch (User.Faction)
+            {
+                case Factions.Blue:
+                    imageBorderGradient = "border-image: linear-gradient(to right, #004cff, #00ffe7);";
+                    primaryColor = "#0051fe";
+                    secondaryColor = "#00caee";
+                    break;
+                case Factions.Red:
+                    imageBorderGradient = "border-image: linear-gradient(to right, #640f0f, #f02d2d);";
+                    primaryColor = "#b50000";
+                    secondaryColor = "#ff2929";
+                    break;
+                case Factions.Green:
+                    imageBorderGradient = "border-image: linear-gradient(to right, #01952d, #73f445);";
+                    primaryColor = "#0d9f30";
+                    secondaryColor = "#59ca32";
+                    break;
+                case Factions.Yellow:
+                    imageBorderGradient = "border-image: linear-gradient(to right, #ffb400, #f7ff00);";
+                    primaryColor = "#f7ff00";
+                    secondaryColor = "#ffb400";
+                    break;
+            }
         }
     }
 }
