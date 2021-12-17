33fd29e241dc46ed59b62b3a6aa7010c64ee6ee4
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2021-12-17 19:00:15 +0200

    Add validated form to editPage
    Created EditUserModel class, it takes care of input given in eidittžProfile page validation.

---
+++ Encounter Me/Models/EditUserModel.cs
@@ -0,0 +1,28 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Encounter_Me.Models
+{
+    public class EditUserModel
+    {
+        [StringLength(30, ErrorMessage = "Username must be shorter than 30 characters")]
+        public string UserName { get; set; }
+
+        [RegularExpression(@"^\p{Lu}[ \p{L}'-]*[\p{Ll}]$",
+            ErrorMessage = "Name must start with a capital letter, and have only letters")]
+
+        public string FirstName { get; set; }
+        [RegularExpression(@"^\p{Lu}[ \p{L}'-]*[\p{Ll}]$",
+            ErrorMessage = "Surname must start with a capital letter and have only letters")]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Url]
+        public string PhotoUrl { get; set; }
+
+        //Password?
+
+    }
+}
